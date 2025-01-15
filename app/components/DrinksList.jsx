43d84b4b646ca9bdeed7aaec2d2bdce46a54e4c0
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-15 15:08:06 +0500

    fill, priority and sizes properties of <Image>

--- app/components/DrinksList.jsx
+++ app/components/DrinksList.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 const DrinksList = ({ drinks }) => {
@@ -9,15 +10,17 @@ const DrinksList = ({ drinks }) => {
             href={`/drinks/${drink.idDrink}`}
             className="text-xl font-medium w-full"
           >
-            <img
-              src={drink.strDrinkThumb}
-              alt={drink.strDrink}
-              className="rounded w-8"
-            />
             <h1 className="text-xl w-28">{drink.strDrink}</h1>
             <h2 className="text-xs w-16">{drink.strCategory}</h2>
             <br />
             <p className="text-sm truncate">{drink.strInstructions}</p>
+            <Image
+              src={drink.strDrinkThumb}
+              alt={drink.strDrink}
+              className="rounded-md object-cover opacity-50"
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
+            />
           </Link>
         </li>
       ))}
