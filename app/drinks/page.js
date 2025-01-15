d70a5f810f9802a90fb7c1bd9b8539de5b4d6f53
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-15 13:45:09 +0500

    challenge: fetch data in /drinks (Server Component, async)

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,4 +1,9 @@
-const DrinksPage = () => {
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
+
+const DrinksPage = async () => {
+  const data = await fetch(url).then((res) => res.json())
+  console.log(data)
+
   return <div>DrinksPage</div>
 }
 
