ee047eeb62ef087c99fe0dbe42a1e860e2a9fd00
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-15 13:37:50 +0500

    challenge: setup basic homepage (Server Component) + counter logic in /client (Client Component)

--- app/client/page.js
+++ app/client/page.js
@@ -1,5 +1,25 @@
+'use client'
+
+import { useState } from 'react'
+
 const ClientPage = () => {
-  return <div>ClientPage</div>
+  const [count, setCount] = useState(0)
+  console.log('Running on client-side')
+
+  return (
+    <div>
+      <h1 className="text-7xl font-bold mb-4">{count}</h1>
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => {
+          setCount((c) => c + 1)
+        }}
+      >
+        Increment
+      </button>
+    </div>
+  )
 }
 
 export default ClientPage
--- app/page.js
+++ app/page.js
@@ -1,16 +1,13 @@
 import Link from 'next/link'
 
 const HomePage = () => {
+  console.log('Running on server-side')
+
   return (
     <div>
-      <h1 className="text-7xl">HomePage</h1>
-
-      <Link href={'/about'} className="text-2xl">
-        About Page
-      </Link>
-      <br />
-      <Link href={'/contact'} className="text-2xl">
-        Contact Page
+      <h1 className="text-5xl mb-8 font-bold">Learn Next.js</h1>
+      <Link href="/client" className="btn btn-accent capitalize">
+        get started
       </Link>
     </div>
   )
