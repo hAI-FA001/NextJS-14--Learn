09547bcaf7c0a607af785207017aefae575eb226
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-15 14:54:31 +0500

    static images, Next.js <Image> + optimizations

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,4 +1,8 @@
 import Link from 'next/link'
+import drinkImg from './pexels-tasveerwala-2842876.jpg'
+import Image from 'next/image'
+
+// console.log(drinkImg)  // is an object
 
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php'
 
@@ -7,7 +11,7 @@ const SingleDrinkPage = async ({ params }) => {
   const id = (await params).id
 
   const data = await fetch(`${url}?i=${id}`).then((res) => res.json())
-  console.log(data)
+  //   console.log(data)
   const drink = data.drinks[0]
 
   return (
@@ -18,8 +22,15 @@ const SingleDrinkPage = async ({ params }) => {
       <h1 className="text-xl w-32">{drink.strDrink}</h1>
       <h2 className="text-xs w-16">{drink.strCategory}</h2>
       <br />
+      <Image
+        src={drinkImg}
+        className="w-48 h-48 rounded-lg"
+        alt={drink.strDrink}
+      />
       <p className="text-sm truncate min-w-96">{drink.strInstructions}</p>
-      <img src={drink.strDrinkThumb} alt={drink.strDrink} />
+      {/* <img src={drink.strDrinkThumb} alt={drink.strDrink} /> */}
+      {/* <img src={drinkImg.src} alt="" /> */}{' '}
+      {/* very slow + layout shifting */}
     </div>
   )
 }
