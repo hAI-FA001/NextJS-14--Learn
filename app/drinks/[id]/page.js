d2e98422e6a48434419c3e5b4565ad46b77bb424
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-15 14:40:09 +0500

    challenge: single drink page - fetch with id from dynamic route params

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,9 +1,20 @@
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php'
+
 const SingleDrinkPage = async ({ params }) => {
   console.log(await params)
+  const id = (await params).id
+
+  const data = await fetch(`${url}?i=${id}`).then((res) => res.json())
+  console.log(data)
+  const drink = data.drinks[0]
 
   return (
     <div>
-      <h1 className="text-7xl">Drink ID - {(await params).id}</h1>
+      <h1 className="text-xl w-32">{drink.strDrink}</h1>
+      <h2 className="text-xs w-16">{drink.strCategory}</h2>
+      <br />
+      <p className="text-sm truncate min-w-96">{drink.strInstructions}</p>
+      <img src={drink.strDrinkThumb} alt={drink.strDrink} />
     </div>
   )
 }
