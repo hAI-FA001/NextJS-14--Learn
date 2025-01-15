15dc5d2db9aaf356232a91f2044cc7914c23d63a
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-15 14:39:50 +0500

    fix image + styling

--- app/components/DrinksList.jsx
+++ app/components/DrinksList.jsx
@@ -2,18 +2,22 @@ import Link from 'next/link'
 
 const DrinksList = ({ drinks }) => {
   return (
-    <ul className="menu menu-vertical pl-0">
+    <ul className="menu menu-vertical pl-0 w-fit">
       {drinks.map((drink) => (
         <li key={drink.idDrink}>
           <Link
             href={`/drinks/${drink.idDrink}`}
-            className="text-xl font-medium"
+            className="text-xl font-medium w-full"
           >
-            <h1 className="text-xl w-32">{drink.strDrink}</h1>
+            <img
+              src={drink.strDrinkThumb}
+              alt={drink.strDrink}
+              className="rounded w-8"
+            />
+            <h1 className="text-xl w-28">{drink.strDrink}</h1>
             <h2 className="text-xs w-16">{drink.strCategory}</h2>
             <br />
-            <p className="text-sm truncate min-w-96">{drink.strInstructions}</p>
-            <img src={drink.strImageSource} alt={drink.strDrink} />
+            <p className="text-sm truncate">{drink.strInstructions}</p>
           </Link>
         </li>
       ))}
