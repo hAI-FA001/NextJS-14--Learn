2646dace89393b327d8a0e753f5f301a92625143
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-16 15:10:51 +0500

    basic prisma example - insert, fetch and show data

--- app/prisma-example/page.js
+++ app/prisma-example/page.js
@@ -1,5 +1,34 @@
-const PrismaExamplePage = () => {
-  return <div>PrismaExamplePage</div>
+import prisma from '@/utils/db'
+
+const prismaHandlers = async () => {
+  await prisma.task.create({
+    data: {
+      content: 'Wake Up',
+    },
+  })
+
+  const allTasks = await prisma.task.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
+  return allTasks
+}
+
+const PrismaExamplePage = async () => {
+  const tasks = await prismaHandlers()
+
+  return (
+    <div>
+      <h1 className="text-7xl">Prisma Example</h1>
+      {tasks.map((t) => (
+        <h2 key={t.id} className="text-xl py-2">
+          {t.content}
+        </h2>
+      ))}
+    </div>
+  )
 }
 
 export default PrismaExamplePage
