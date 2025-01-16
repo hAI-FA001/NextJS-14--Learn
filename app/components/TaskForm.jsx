f7a422f403445ea5ed89a149eba7fa9af8789545
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-16 15:55:17 +0500

    finish up TaskForm: Server Actions "use server", revalidatePath

--- app/components/TaskForm.jsx
+++ app/components/TaskForm.jsx
@@ -1,8 +1,32 @@
+import prisma from '@/utils/db'
+import { revalidatePath } from 'next/cache'
+
+const createTask = async (formData) => {
+  'use server'
+  const content = formData.get('content')
+  console.log('From Server Action: ', content)
+
+  await prisma.task.create({ data: { content } })
+
+  revalidatePath('/tasks')
+}
+
 const TaskForm = async () => {
   return (
-    <div>
-      <h1 className="text-xl">TaskForm</h1>
-    </div>
+    <form action={createTask}>
+      <div className="join w-full">
+        <input
+          type="text"
+          name="content"
+          placeholder="Type here"
+          className="input input-bordered join-item w-full"
+          required
+        />
+        <button type="submit" className="btn btn-primary join-item">
+          Create
+        </button>
+      </div>
+    </form>
   )
 }
 export default TaskForm
